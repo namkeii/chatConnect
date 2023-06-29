8cab3a25922d9a23500834e45b095a13c7354b0d
namkeii <namanovarma@gmail.com>
2023-06-30 03:52:01 +0530

    all done!

--- app/src/main/java/com/example/chatconnect/view/AuthenticationOption.kt
+++ app/src/main/java/com/example/chatconnect/view/AuthenticationOption.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
