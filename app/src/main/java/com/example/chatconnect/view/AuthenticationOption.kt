763a39d201eccd8da840cdb0be6d5b60a402a905
namkeii <namanovarma@gmail.com>
2023-06-30 03:04:05 +0530

    Authentication done!

---
+++ app/src/main/java/com/example/chatconnect/view/AuthenticationOption.kt
@@ -0,0 +1,40 @@
+package com.example.chatconnect.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.chatconnect.view.ui.theme.ChatConnectTheme
+
+
+@Composable
+fun AuthenticationView(register: () -> Unit, login: () -> Unit) {
+    ChatConnectTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Title(title = "⚡️ Chat Connect")
+                Buttons(title = "Register", onClick = register, backgroundColor = Color.Blue)
+                Buttons(title = "Login", onClick = login, backgroundColor = Color.Magenta)
+            }
+        }
+    }
+}
