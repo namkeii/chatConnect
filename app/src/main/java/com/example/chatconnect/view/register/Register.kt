6b988c3f122a2cb8b1ae3a000afc3c70a3862487
Prachi <imprachi2015@gmail.com>
2023-06-30 12:06:37 +0530

    Register.kt created under register pkg of view pkg

--- app/src/main/java/com/example/chatconnect/view/register/Register.kt
+++ app/src/main/java/com/example/chatconnect/view/register/Register.kt
@@ -1,5 +1,5 @@
 package com.example.chatconnect.view.register
-//register
+//register_pkg
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
